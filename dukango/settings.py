368d96a8db2a87b1675a8dd40a50b289559607dd
Raja <clouddc@Raja.local>
2026-01-03 15:47:34 +0530

    Disable static post-processing to fix Render collectstatic

--- dukango/settings.py
+++ dukango/settings.py
@@ -96,7 +96,7 @@ STATIC_URL = '/static/'
 
 STORAGES = {
     'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
-    'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage'},
+    'staticfiles': {'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage'},
 }
 
 WHITENOISE_MANIFEST_STRICT = False
