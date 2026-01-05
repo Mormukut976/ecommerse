4355bfe3c5a64a0e13a61aaa6b7db39d0e6b2fda
Raja <clouddc@Raja.local>
2026-01-05 17:12:58 +0530

    Add healthz endpoint

--- dukango/urls.py
+++ dukango/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,8 +9,13 @@ admin.site.site_title = 'Shope in home Admin'
 admin.site.index_title = 'Dashboard'
 admin.site.enable_nav_sidebar = False
 
+
+def healthz(request):
+    return HttpResponse('ok', content_type='text/plain')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthz/', healthz, name='healthz'),
     path('accounts/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
